a32b426d00425e5aa98d19e133bfcbc22d9ccd46
lii-nk <sam@arbor.me.uk>
2025-06-17 20:13:08 +0100

    corrected error in code

--- main.py
+++ main.py
@@ -5,18 +5,16 @@ import pygame
 from constants import *
 
 def main():
-    print('Starting Asteroids!')
-    print(f'Screen width: {SCREEN_WIDTH}')
-    print(f'Screen height: {SCREEN_HEIGHT}')
     pygame.init()
-    x = True
-    while x == True:
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-            screen.fill('black')
-            pygame.display.flip()
+
+        screen.fill("black")
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
